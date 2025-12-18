ecbf0e007db3f517d528a6f421089298c607caae
Matias Casiba <casibagabriel@gmail.com>
2025-12-18 14:43:00 -0300

    Armo la estructura de la página Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Pagina de inicio</h1>
+      <p>banner y productos destacados</p>
+
+      <div className="placeholder">
+        <h2>Banner</h2>
+        <p>Imagen promocional de helados</p>
+      </div>
+
+      <div className="placeholder">
+        <h2>Productos destacados</h2>
+        <p>grid de tarjetas de productos</p>
+      </div>
+    </div>
   )
 }
 
