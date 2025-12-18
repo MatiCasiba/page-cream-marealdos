cf4946b2d1af4d676fef7b860203cf569c3c7c85
Matias Casiba <casibagabriel@gmail.com>
2025-12-18 15:00:56 -0300

    Armo la estructura de la página Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,21 @@
+import './Contact.scss'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <h1>Contacto</h1>
+      <p>Formulario de contacto</p>
+
+      <div className='placeholder'>
+        <h2>Información de contacto</h2>
+        <p>Dirección, teléfono, email</p>
+      </div>
+
+      <div className='placeholder'>
+        <h2>Formulario</h2>
+        <p>Formulario para que los clientes se contacten</p>
+      </div>
+    </div>
   )
 }
 
