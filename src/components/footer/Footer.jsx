61bda22cfaef01b010805d0ce3651ab8da0d8a2f
Matias Casiba <casibagabriel@gmail.com>
2025-12-19 14:44:09 -0300

    Desarrollo del footer (logo y info)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,27 @@
 
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="footer-content">
+
+      <div className="footer-section">
+        <div className="footer-logo">
+          <img 
+            src="../../public/logos/marelados.png" 
+            alt="marelados logo"
+            className="footer-logoImg" 
+          />
+          <h3 className="footer-title"></h3>
+        </div>
+        <p className="footer-desciption">
+          Ola de sabor en cada bocado.
+        </p>
+      </div>
+      </div>
+    </footer>
   )
 }
 
