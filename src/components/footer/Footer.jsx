d48dbf1f7a0a90080786da9f482f2d1877f0a649
Matias Casiba <casibagabriel@gmail.com>
2025-12-19 14:56:33 -0300

    Desarrollo del footer (redes sociales)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa"
 
 
 const Footer = () => {
@@ -7,19 +8,31 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
 
-      <div className="footer-section">
-        <div className="footer-logo">
-          <img 
-            src="../../public/logos/marelados.png" 
-            alt="marelados logo"
-            className="footer-logoImg" 
-          />
-          <h3 className="footer-title"></h3>
+        <div className="footer-section">
+          <div className="footer-logo">
+            <img 
+              src="../../public/logos/marelados.png" 
+              alt="marelados logo"
+              className="footer-logoImg" 
+            />
+            <h3 className="footer-title"></h3>
+          </div>
+          <p className="footer-desciption">
+            Ola de sabor en cada bocado.
+          </p>
+
+          <div className="social-media">
+            <a href="" target="_blank" rel="" aria-label="Facebook">
+              <FaFacebook className="social-icon" />
+            </a>
+            <a href="" target="_blank" rel="" aria-label="Instagram">
+              <FaInstagram className="social-icon" />
+            </a>
+            <a href="" target="_blank" rel="" aria-label="WhatsApp">
+              <FaWhatsapp className="social-icon" />
+            </a>
+          </div>
         </div>
-        <p className="footer-desciption">
-          Ola de sabor en cada bocado.
-        </p>
-      </div>
       </div>
     </footer>
   )
